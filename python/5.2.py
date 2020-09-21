395bb28be6c653f7965a0d69b93ec5fa4ad06d88
wmh010810 <hryzwmh@sjtu.edu.cn>
2020-09-21 23:31:29 +0800

    if

--- python/5.2.py
+++ python/5.2.py
@@ -13,3 +13,17 @@ print(19 < 21)
 print(19 <= 21)
 print(19 > 21)
 print(19 >= 21)
+# 检查多个条件
+# 且
+print(22 >= 21 and 18 >= 21)
+print(22 >= 21 and 22 >= 21)
+# 或
+print(21 >= 22 or 18 >= 22)
+print(18 >= 22 or 18 >= 22)
+# 检查特定值是否在列表中
+requested_toopings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toopings)
+print('pepperoni' in requested_toopings)
+# 检查特定值是否不在列表中
+banned_users = ['andrew', 'carolina', 'david']
+print('marie' not in banned_users)
