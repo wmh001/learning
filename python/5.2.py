b31f9591cda7965944ce7f968e3d7f90d4e56cef
wmh010810 <hryzwmh@sjtu.edu.cn>
2020-09-21 23:06:42 +0800

    if

---
+++ python/5.2.py
@@ -0,0 +1,15 @@
+# 检查是否相等
+print('bwm' == 'bwm')
+print('audi' == 'bmw')
+# 检查大小写是否相同
+print('Audi' == 'audi')
+print('Audi'.lower() == 'audi')
+# 检查是否不相等
+print('mushroom' != 'anchovies')
+# 比较数字
+print(18 == 18)
+print(17 != 42)
+print(19 < 21)
+print(19 <= 21)
+print(19 > 21)
+print(19 >= 21)
