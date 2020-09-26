5bf3efaa3d73b49a4e6d16f9a034c50ae8327844
wmh010810 <hryzwmh@sjtu.edu.cn>
2020-09-26 23:40:15 +0800

    last2.2

--- python物理建模初学者指南/2.2.py
+++ python物理建模初学者指南/2.2.py
@@ -29,6 +29,9 @@ a = np.zeros((2, 3, 4))
 print(a)
 # np.size()计算数组尺寸
 print(np.size(a))
+# np.size(a,b)计算数组a第b行的尺寸
+A = np.zeros((2, 5))
+print(np.size(A, 1))
 # np.shape()计算数组规格
 print(np.shape(a))
 # np.sum()求元素和
@@ -100,9 +103,74 @@ print(B)
 # [a:b]从第a个元素到第b-1个元素
 print(B[2:5])
 # [a:b:c]从第a个元素到第b-1个元素，公差为c
+# 最后一个元素为满足a+n*c<b的最大整数
 print(B[2:7:2])
 print(B[2:8:2])
 # 从第零个元素开始，第一个参数可以省略
 print(B[:5])
 # 到最后一个元素结束，第二个参数可以省略
 print(B[5:])
+# 多维数组切片
+A = np.eye(4)
+print(A)
+# [a:b:c,d]在第d行从第a个元素到第b-1个元素，公差维c
+x = A[1:3:1, 1]
+print(x)
+# 多维数组分成多个一维数组
+A = np.zeros((2, 4))
+A[0] = np.array(range(1, 5))
+A[1] = np.array(range(5, 9))
+print(A)
+# 按行分
+x = A[0, :]
+y = A[1, :]
+print(x)
+print(y)
+# 按列分
+h = A[:, 0]
+g = A[:, 1]
+print(h)
+print(g)
+# 切片时使用负索引值
+print(x[-3:])
+# 切片赋值
+A = np.zeros(10)
+A[1:4] = np.ones(3)
+print(A)
+# 数组展平
+A = np.array([[1, 2], [1, 2]])
+print(A)
+# .flatten()创建新数组
+a = A.flatten()
+print(a)
+a[1] = 3
+print(A)
+# np.ravel(A)创建指向同一数据区但形状不同的数据结构
+b = np.ravel(A)
+print(b)
+b[2] = 3
+print(A)
+# 更改数组形状但不创建新数组
+a = np.arange(12)
+print(a)
+# np.reshape(a,(,))将A改变维元组的形状
+b = np.reshape(a, (3, 4))
+print(b)
+# .reshape(())将调用对象改编为元组的形状
+c = b.reshape((2, 6))
+print(c)
+d = b.reshape((2, 3, 2))
+print(d)
+print(d)
+# 未创建新数组
+d[1][1][1] = 12
+print(a)
+# 以列表和数组未索引
+a = np.arange(10, 21)
+print(a)
+b = [2, 4, 5]
+print(a[b])
+# 以条件值做索引
+a = np.arange(-10, 11)
+print(a)
+print(a[abs(a) < 5])
