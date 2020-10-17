d6cf4e637ce89784e55f8b5ab66ea6d2a161d098
wmh010810 <hryzwmh@sjtu.edu.cn>
2020-10-17 23:44:54 +0800

    字典

---
+++ Python编程从入门到实践/6.2.py
@@ -0,0 +1,8 @@
+# 使用字典
+
+# 根据键字典的访问
+alien_0 = {'color': 'green'}
+print(alien_0['color'])
+alien_0 = {'color': 'green', 'points': 5}
+new_points = alien_0['points']
+print('You just earned ' + str(new_points) + ' points!')
