2cadb40a82d61d5fad10ed91969b27527a942f20
wmh010810 <hryzwmh@sjtu.edu.cn>
2021-01-16 22:02:35 +0800

    while

---
+++ Python编程从入门到实践/7.2.py
@@ -0,0 +1,16 @@
+# while循环简介
+
+# 使用while循环
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# 让用户选择何时退出
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'puit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
