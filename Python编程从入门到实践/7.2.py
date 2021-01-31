1e741c7d63b52939b90389ab469485c1886f409b
wmh010810 <hryzwmh@sjtu.edu.cn>
2021-01-31 22:02:18 +0800

    while

--- Python编程从入门到实践/7.2.py
+++ Python编程从入门到实践/7.2.py
@@ -24,3 +24,16 @@ while actice:
         actice = False
     else:
         print(message)
+
+# 使用break退出循环
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'puit' when you are finished.) "
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
+
+# 在循环中使用continue
