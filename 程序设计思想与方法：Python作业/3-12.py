5853f349a8714baf90b67fc3bb6e3036e2309612
wmh010810 <hryzwmh@sjtu.edu.cn>
2021-03-15 14:48:18 +0800

    作业

---
+++ 程序设计思想与方法：Python作业/3-12.py
@@ -0,0 +1,24 @@
+while True:
+    weight = input("请输入体重（单位：千克）：")
+    Weight = float(weight)
+    if Weight > 0:
+        break
+    else:
+        print("请输入正数！")
+while True:
+    height = input("请输入身高（单位：米）：")
+    Height = float(height)
+    if Height > 0:
+        break
+    else:
+        print("请输入正数！")
+BMI = Weight / Height**2
+print("BMI为%.2f" % BMI)
+if BMI < 19:
+    print("轻体重")
+elif BMI < 25:
+    print("健康体重")
+elif BMI < 28:
+    print("超重")
+else:
+    print("肥胖")
