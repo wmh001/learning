b2e55a388c3e5e6f386e4d8a18f5774f85daa3f2
wmh010810 <hryzwmh@sjtu.edu.cn>
2021-03-15 17:05:55 +0800

    作业

--- 程序设计思想与方法：Python作业/3-20.py
+++ 程序设计思想与方法：Python作业/3-20.py
@@ -1,5 +1,9 @@
 sum = 0
-for i in range(1, 1001):
-    if (i % 3 == 0) and (i % 5 != 0):
+# 取1到1000中3的倍数依次循环
+for i in range(0, 1001, 3):
+    # 是否能被不能被5整除
+    if (i % 5 != 0):
+        # 求和
         sum += i
+# 输出
 print(sum)
