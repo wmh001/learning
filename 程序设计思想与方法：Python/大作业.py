fad3730dab290cd1012de565569f3c3a86c82f7d
wmh001 <hryzwmh@sjtu.edu.cn>
2021-05-20 00:23:35 +0800

    原型

---
+++ 程序设计思想与方法：Python/大作业.py
@@ -0,0 +1,13 @@
+# -*-coding:utf-8-*-
+
+import tkinter
+
+
+class deflectionEquationCalculator:
+    def __init__(self):
+        self.root = tkinter.Tk()
+
+        self.root.mainloop()
+
+
+d = deflectionEquationCalculator()
